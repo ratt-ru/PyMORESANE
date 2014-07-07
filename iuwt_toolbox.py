5545574988ca2de3393d7c0d61162291189ceb63
JSKenyon <jskenyon007@gmail.com>
2014-07-07 14:21:08 +0200

    Commit of new toolbox script. Currently contains thresholding functionality. Confirmed working. This is much more elegant than the original but will require some changes to the main script as it has much less complicated layout.

---
+++ iuwt_toolbox.py
@@ -0,0 +1,58 @@
+import numpy as np
+from scipy import ndimage
+
+import pyfits
+import iuwt
+import pylab as pb
+import time
+
+def threshold(in1, sigma_level=4):
+    """
+    This function performs the thresholding of the values in array in1 based on the estimated standard deviation
+    given by the MAD (median absolute deviation) estimator about zero.
+
+    INPUTS:
+    in1             (no default):   The array which is to be thresholded.
+    sigma_level     (no default):   The number of estimated deviations at which thresholding is to occur.
+    """
+
+    out1 = np.empty_like(in1)
+
+    # The conditional here ensures that the function works even when only one scale is considered. Both cases are the
+    # same: the MAD estimator is calculated and then the resulting value is used to threshold the input. NOTE: This
+    # discards all negative coefficients.
+
+    if len(in1.shape)==2:
+        threshold_level = np.median(np.abs(in1))/0.6745                     # MAD estimator for normal distribution.
+        out1 = np.where(in1<(sigma_level*threshold_level),0,1)*in1
+    else:
+        for i in range(in1.shape[0]):
+            threshold_level = np.median(np.abs(in1[i,:,:]))/0.6745          # MAD estimator for normal distribution.
+            out1[i,:,:] = np.where(in1[i,:,:]<(sigma_level*threshold_level),0,1)*in1[i,:,:]
+
+    return out1
+
+if __name__=="__main__":
+    img_hdu_list = pyfits.open("3C147.fits")
+    psf_hdu_list = pyfits.open("3C147_PSF.fits")
+
+    dirty_data = (img_hdu_list[0].data[0,0,:,:]).astype(np.float32)
+    psf_data = (psf_hdu_list[0].data[0,0,:,:]).astype(np.float32)
+
+    img_hdu_list.close()
+    psf_hdu_list.close()
+
+    decomposition = iuwt.iuwt_decomposition(dirty_data,2)
+    print decomposition.shape
+
+    t = time.time()
+    thresh_decom = threshold(decomposition, 3)
+    print "Time:", time.time() - t
+
+    print dirty_data
+
+    # pb.imshow(decomposition[-2,:,:])
+    # pb.show()
+    # pb.imshow(thresh_decom[-2,:,:])
+    # pb.show()
+
