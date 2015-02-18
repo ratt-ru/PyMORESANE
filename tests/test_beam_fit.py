2b9a6abc57e958ea0bcaa5dc0917510e76f7dbfe
Gijs Molenaar <gijs@pythonic.nl>
2015-02-18 14:37:00 +0200

    add test skeleton

---
+++ tests/test_beam_fit.py
@@ -0,0 +1,6 @@
+import pymoresane.beam_fit
+import unittest
+
+
+class TestBeamFit(unittest.TestCase):
+    pass
---
+++ tests/test_iuwt_toolbox.py
@@ -0,0 +1,6 @@
+import pymoresane.iuwt_toolbox
+import unittest
+
+
+class TestIuwtToolbox(unittest.TestCase):
+    pass
